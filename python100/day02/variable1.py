799194c7c7a59175d6952428f74a16d8dea63a9c
hjr <1627383642@qq.com>
2018-07-09 11:51:03 +0800

    Python2语言元素 -- 2018.07.09

---
+++ python100/day02/variable1.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+# 使用变量保存数据并进行算数运算
+a = 321
+b = 123
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+print(a // b)
+print(a % b)
+print(a ** b)
---
+++ python100/day02/variable2.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+# 使用input函数输入
+# 使用int()进行类型转换
+# 用占位符格式化输出的字符串
+
+a = int(input('a = '))
+b = int(input('b = '))
+
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %d' % (a, b, a / b))
+
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
